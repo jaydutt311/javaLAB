9eb9700d3f0cdfedf30ed31d4b13732d88f5f4ae
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-04-09 10:10:15 +0530

    Create fourth.java

---
+++ fourth.java
@@ -0,0 +1,11 @@
+//Write a program to perform the addition of n nos. by taking inputs from command line.
+import java.util.*;
+public class fourth {
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            int a  =sc.nextInt();
+            int b  =sc.nextInt();
+            int sum = a+b;
+            System.out.println(sum);
+        }
+    }
