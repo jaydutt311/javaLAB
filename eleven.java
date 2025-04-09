a4e69b638f9e46282fe66d309e2dda98a2069545
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-04-09 10:31:04 +0530

    Create eleven.java

---
+++ eleven.java
@@ -0,0 +1,19 @@
+//Write a program to show the use of Boolean data type.
+public class eleventh {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is fish tasty? " + isFishTasty);
+        if (isJavaFun) {
+            System.out.println("Let's keep learning Java!");
+        } else {
+            System.out.println("Maybe try a different language.");
+            int a = 10;
+            int b = 20;
+            boolean comparisonResult = a < b;
+
+            System.out.println("Is a less than b? " + comparisonResult);
+        }
+    }
+}
