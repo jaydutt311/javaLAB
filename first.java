6e02b05e79f70a7f463b29ad1b0e2a9ee812d3e2
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-04-09 09:41:01 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,10 @@
+//1 	Write a program to display your information using print () method. 
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("Name: JAYDUTT UPADHYAY");
+        System.out.println("Age: 23");
+        System.out.println("Email: jayduttupadhyay9399@gmail.com");
+        System.out.println("Course: Computer Science");
+        System.out.println("Hobby: bike riding");
+    }
+}
